be2ac03e448396095ca75ed0990b1340d4bbd047
Manan Kandpal <manank.7409@gmail.com>
2026-06-01 08:59:57 +0530

    feat: add string practice problems and study notes for day 3 DSA coursework

--- DSA/Day 3 - 29 May/aq3.java
+++ DSA/Day 3 - 29 May/aq3.java
@@ -1,25 +1,25 @@
 //check palindrome
 
 import java.util.Scanner;
+
 class Palindrome {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().trim();
-        
-        int l =0; r = str.length() - 1;
-        while(l<r){
-            if(str.charAt(l)!=str.charAt(r)){
+
+        int l = 0, r = str.length() - 1;
+        while (l < r) {
+            if (str.charAt(l) != str.charAt(r)) {
                 break;
             }
             l++;
             r--;
         }
 
-        if(l>=r){
-            System.out.println(str+" is a Palindrome");
-        }
-        else{
-            System.out.println(str+" is not a Palindrome");
+        if (l >= r) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is not a Palindrome");
         }
 
         sc.close();
